23c5263148c24edf7f1f5f301df8994c987df05f
BenBxn <dessortbxn@gmail.com>
2022-10-04 21:14:42 +0200

    Correction server & Commentaires

--- Back/server.js
+++ Back/server.js
@@ -1,12 +1,15 @@
 //Sources 
 //https://openclassrooms.com/fr/courses/6390246-passez-au-full-stack-avec-node-js-express-et-mongodb/6466277-creez-une-application-express#/id/r-6466276
-const http = require("http");
+// lancer serveur sur http localhost 3000
+
+//Import module http node
+const http = require("http"); 
+//Import Fichier app.js
 const app = require("./app");
 
+//Import package Dotenv
 const dotenv = require("dotenv");
 dotenv.config();
-
-
 
 const normalizePort = val => {
     const port = parseInt(val, 10);
@@ -20,19 +23,21 @@ const normalizePort = val => {
     return false;
 };
 
+//Deffinir PORT
 const port = normalizePort(process.env.PORT || "3000");
+//Express port
 app.set("port", port);
 
-
-
-
+//Rechercher erreur 
 const errorHandler = error => {
+    //si serveur KO
     if (error.syscall !== 'listen') {
         throw error;
     }
     const address = server.address();
     const bind = 
         typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    //si error code
     switch (error.code) {
         case 'EACCES':
             console.error(bind + ' requires elevated privileges.');
@@ -50,8 +55,9 @@ const errorHandler = error => {
 //Connecter app au serveur via HTTP
 const server = http.createServer(app);
 
-//Ecouter d'événements 
+//Ecouter événements 
 //Indiquer Port utilisé par le serveur
+//si serveur KO > errorHandler
 server.on('error', errorHandler);
 
 server.on('listening', () => {
